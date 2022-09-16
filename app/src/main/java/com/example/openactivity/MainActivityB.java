572e0bef2fb483d77a37e8634deee48ced275d72
trandanghao2002 <trandanghao09@gmail.com>
2022-09-16 10:50:13 +0700

    first commit

---
+++ app/src/main/java/com/example/openactivity/MainActivityB.java
@@ -0,0 +1,15 @@
+package com.example.openactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_b);
+    }
+}
+
